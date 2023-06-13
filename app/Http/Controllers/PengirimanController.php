3858824f6c5cf7c2531f45de53a4b056460d9a3e
arifnurulhakim <arifnhakim23@gmail.com>
2023-06-13 14:42:17 +0700

    base64

--- app/Http/Controllers/PengirimanController.php
+++ app/Http/Controllers/PengirimanController.php
@@ -77,32 +77,32 @@ class PengirimanController extends Controller
     }
 
 
-    
-public function generateQrCode($id)
-{
-    try {
-        $pengiriman = Pengiriman::findOrFail($id);
-        
-        // Generate QR code from URL
-        $url = 'https://sistemkodeposkominfo.com/index.html#/Dashboard';
-        $qrCode = QrCode::size(300)->generate($url);
-        
-        // Convert QR code image to base64
-        $base64 =base64_encode($qrCode);
-        $database64 = $base64;
-        $qrCodeData = [
-            'pengirimanData' => $pengiriman,
-        ];
 
-        return response()->json([
-            'status' => 'success',
-            'qrCode' => $database64,
-            'data' => $qrCodeData,
-        ], 200);
-    } catch (\Exception $e) {
-        return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+    public function generateQrCode($id)
+    {
+        try {
+            $pengiriman = Pengiriman::findOrFail($id);
+            
+            // Generate QR code from data
+            $qrCode = QrCode::format('png')->size(300)->generate($pengiriman->url);
+            
+            // Convert QR code image to base64
+            $base64 = base64_encode($qrCode);
+            
+            $qrCodeData = [
+                'pengirimanData' => $pengiriman,
+            ];
+    
+            return response()->json([
+                'status' => 'success',
+                'qrCode' => $base64,
+                'data' => $qrCodeData,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
     }
-}
+    
     
     
 }
