16abf8b81194226ab66c715c4aef5fcd3b5102ee
arifnurulhakim <arifnhakim23@gmail.com>
2023-06-14 19:59:01 +0700

    123123

--- app/Http/Controllers/PengirimanController.php
+++ app/Http/Controllers/PengirimanController.php
@@ -88,7 +88,7 @@ public function generateQrCode($id)
         $qrCode = QrCode::size(300)->generate($url);
         
         // Convert QR code image to base64
-        $base64 = 'data:image/png;base64,' . base64_encode($qrCode);
+        $base64 = 'data:image/svg;base64,' . base64_encode($qrCode);
         $img = [
             'base64'=>$base64
         ];
