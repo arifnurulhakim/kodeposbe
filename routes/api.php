614dd2f6dc24b5fc27646e9b4524c82f2ce5dd38
arifnurulhakim <arifnhakim23@gmail.com>
2023-06-15 13:34:46 +0700

    midleware

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\PusherController;
-use App\Http\Controllers\PaymentCallbackController; // => letakkan dibagian atas
-use App\Http\Controllers\OrderController; // => letakkan dibagian atas
-use App\Http\Controllers\ProductController; // => letakkan dibagian atas
-use App\Http\Controllers\KeranjangController; // => letakkan dibagian atas
-use App\Http\Controllers\ProvController; // => letakkan dibagian atas
-use App\Http\Controllers\KabController; // => letakkan dibagian atas
-use App\Http\Controllers\KecController; // => letakkan dibagian atas
-use App\Http\Controllers\DesaController; // => letakkan dibagian atas
-use App\Http\Controllers\KodePosController; // => letakkan dibagian atas
-use App\Http\Controllers\UserLogController; // => letakkan dibagian atas
-use App\Http\Controllers\PengirimanController; // => letakkan dibagian atas
+use App\Http\Controllers\PaymentCallbackController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\KeranjangController;
+use App\Http\Controllers\ProvController;
+use App\Http\Controllers\KabController;
+use App\Http\Controllers\KecController;
+use App\Http\Controllers\DesaController;
+use App\Http\Controllers\KodePosController;
+use App\Http\Controllers\UserLogController;
+use App\Http\Controllers\PengirimanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -58,14 +58,72 @@ Route::controller(AuthController::class)->group(function () {
     // Route login tidak perlu middleware auth:api
     Route::post('/login', 'login');
     Route::post('/register', 'register');
-    Route::get('/getProfile', 'getProfile');
-    Route::get('/getUser/{id}', 'getUser');
-    Route::get('/getAlluser', 'getAlluser');
-    Route::delete('/deleteUser/{id}', 'deleteUser');
-    Route::post('/updateUser/{id}', 'updateUser');
-    Route::post('/logout', 'logout');
-    Route::get('/exportCSV', 'exportCSV');
+});
 
+Route::middleware('auth:api')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/getProfile', 'getProfile');
+        Route::get('/getUser/{id}', 'getUser');
+        Route::get('/getAlluser', 'getAlluser');
+        Route::delete('/deleteUser/{id}', 'deleteUser');
+        Route::post('/updateUser/{id}', 'updateUser');
+        Route::post('/logout', 'logout');
+        Route::get('/exportCSV', 'exportCSV');
+    });
+
+    Route::controller(KodePosController::class)->group(function () {
+        Route::get('/kode-pos', 'index');
+        Route::post('/kode-pos', 'store');
+        Route::get('/kode-pos/{id}', 'show');
+        Route::put('/kode-pos/{id}', 'update');
+        Route::delete('/kode-pos/{id}', 'destroy');
+    });
+
+    Route::controller(PengirimanController::class)->group(function () {
+        Route::get('/pengiriman', 'index');
+        Route::post('/pengiriman', 'store');
+        Route::get('/pengiriman/{id}', 'show');
+        Route::put('/pengiriman/{id}', 'update');
+        Route::delete('/pengiriman/{id}', 'destroy');
+        Route::get('/pengiriman/qrcode/{id}', 'generateQrCode');
+    });
+
+    Route::controller(UserLogController::class)->group(function () {
+        Route::get('/userlog', 'index');
+        Route::get('/userlog/{id}', 'show');
+    });
+
+    Route::controller(ProvController::class)->group(function () {
+        Route::get('/provinsi', 'index');
+        Route::post('/provinsi', 'store');
+        Route::get('/provinsi/{id}', 'show');
+        Route::put('/provinsi/{id}', 'update');
+        Route::delete('/provinsi/{id}', 'destroy');
+    });
+    
+    Route::controller(KabController::class)->group(function () {
+        Route::get('/kabupaten', 'index');
+        Route::post('/kabupaten', 'store');
+        Route::get('/kabupaten/{id}', 'show');
+        Route::put('/kabupaten/{id}', 'update');
+        Route::delete('/kabupaten/{id}', 'destroy');
+    });
+    
+    Route::controller(KecController::class)->group(function () {
+        Route::get('/kecamatan', 'index');
+        Route::post('/kecamatan', 'store');
+        Route::get('/kecamatan/{id}', 'show');
+        Route::put('/kecamatan/{id}', 'update');
+        Route::delete('/kecamatan/{id}', 'destroy');
+    });
+    
+    Route::controller(DesaController::class)->group(function () {
+        Route::get('/desa', 'index');
+        Route::post('/desa', 'store');
+        Route::get('/desa/{id}', 'show');
+        Route::put('/desa/{id}', 'update');
+        Route::delete('/desa/{id}', 'destroy');
+    });
 });
 
 Route::controller(ForgotPasswordController::class)->group(function () {
@@ -85,96 +143,6 @@ Route::controller(ResetPasswordController::class)->group(function () {
 
 Route::post('/reset-first-password', [ResetPasswordController::class, 'resetFirstPassword'])->name('reset-first-password');
 
-Route::post('/sendMessage/{eventName}', [PusherController::class, 'sendMessage'])->name('sendMessage');
-Route::get('/chatList', [PusherController::class, 'chatList']);
-Route::get('/chatList/{id_penerima}', [PusherController::class, 'chatDetail']);
 
-Route::get('/test', [PusherController::class, 'test'])->name('test');
-// Route::get('/orders-show', [OrderController::class, 'show'])->name('orders-show');
-
-Route::resource('orders', OrderController::class);
-Route::resource('product', ProductController::class);
-Route::resource('keranjang', KeranjangController::class);
-// Route::resource('orders', OrderController::class)->only(['index', 'show']);
- 
-Route::post('payments/midtrans-notification', [PaymentCallbackController::class, 'receive']);
-
-// provinsi
-Route::controller(ProvController::class)->group(function () {
-
-    Route::get('/provinsi', 'index');
-    Route::post('/provinsi', 'store');
-    Route::get('/provinsi/{id}', 'show');
-    Route::put('/provinsi/{id}', 'update');
-    Route::delete('/provinsi/{id}', 'destroy');
-
-});
-
-// kabupaten
-Route::controller(KabController::class)->group(function () {
-
-    Route::get('/kabupaten', 'index');
-    Route::post('/kabupaten', 'store');
-    Route::get('/kabupaten/{id}', 'show');
-    Route::put('/kabupaten/{id}', 'update');
-    Route::delete('/kabupaten/{id}', 'destroy');
-
-});
-
-// kecamatan
-Route::controller(KecController::class)->group(function () {
-
-    Route::get('/kecamatan', 'index');
-    Route::post('/kecamatan', 'store');
-    Route::get('/kecamatan/{id}', 'show');
-    Route::put('/kecamatan/{id}', 'update');
-    Route::delete('/kecamatan/{id}', 'destroy');
-
-});
-
-// desa
-Route::controller(DesaController::class)->group(function () {
-
-    Route::get('/desa', 'index');
-    Route::post('/desa', 'store');
-    Route::get('/desa/{id}', 'show');
-    Route::put('/desa/{id}', 'update');
-    Route::delete('/desa/{id}', 'destroy');
-
-});
-
-// kode pos
-Route::controller(KodePosController::class)->group(function () {
-
-    Route::get('/kode-pos', 'index');
-    Route::post('/kode-pos', 'store');
-    Route::get('/kode-pos/{id}', 'show');
-    Route::put('/kode-pos/{id}', 'update');
-    Route::delete('/kode-pos/{id}', 'destroy');
-
-});
-
-// pengiriman
-Route::controller(PengirimanController::class)->group(function () {
-
-    Route::get('/pengiriman', 'index');
-    Route::post('/pengiriman', 'store');
-    Route::get('/pengiriman/{id}', 'show');
-    Route::put('/pengiriman/{id}', 'update');
-    Route::delete('/pengiriman/{id}', 'destroy');
-    Route::get('/pengiriman/qrcode/{id}', 'generateQrCode');
-
-
-});
-
-// pengiriman
-Route::controller(UserLogController::class)->group(function () {
-
-    Route::get('/userlog', 'index');
-    // Route::post('/userlog', 'store');
-    Route::get('/userlog/{id}', 'show');
-    // Route::put('/userlog/{id}', 'update');
-    // Route::delete('/userlog/{id}', 'destroy');
-});
 
 
