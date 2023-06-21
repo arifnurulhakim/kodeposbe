7c09a0d586385c80b0f490fd76a66192acfece29
arifnurulhakim <arifnhakim23@gmail.com>
2023-06-22 02:17:16 +0700

    udpate

--- routes/api.php
+++ routes/api.php
@@ -65,7 +65,7 @@ Route::controller(ProvController::class)->group(function () {
 });
 Route::controller(KodePosController::class)->group(function () {
     Route::get('/kode-pos/kodepos/{kodepos}', 'getbykodepos');
-    Route::get('/kode-pos/provinsi/{provinsi}', 'getbyprovinsi');
+    Route::get('/kode-pos/allprovinsi/{provinsi}', 'getbyprovinsi');
     Route::get('/kode-pos/{provinsi}/{kabupaten}', 'getbykabupaten');
     Route::get('/kode-pos/{provinsi}/{kabupaten}/{kecamatan}', 'getbykecamatan');
     Route::get('/kode-pos/{provinsi}/{kabupaten}/{kecamatan}/{desa}', 'getbydesa');
